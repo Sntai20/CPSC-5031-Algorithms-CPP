99cd4d260819e1aec4623fc2c205f5b875e9f360
Antonio Santana <ansantan@microsoft.com>
2023-11-04 12:03:54 -0700

    .

--- FindMinimumDistance-Finished/Program.cpp
+++ FindMinimumDistance-Finished/Program.cpp
@@ -2,7 +2,7 @@
 
 using namespace std;
 
-// Given an array of n integers. Find the minimum distance between any two occurrences of the minimum integer in the array.
+// Given an array of n integers, find the minimum distance between any two occurrences of the minimum integer in the array.
 // Input: Array A[0..n -1] of numbers.
 // Output: Minimum distance between two of its elements.
 // Reference(s): https://www.geeksforgeeks.org/distance-two-closest-minimum/
--- FindMinimumDistance-Start/Program.cpp
+++ FindMinimumDistance-Start/Program.cpp
@@ -1,9 +1,8 @@
-#include <array>
 #include <iostream>
 
 using namespace std;
 
-// Given an array of n integers. Find the minimum distance between any two occurrences of the minimum integer in the array.
+// Given an array of n integers, find the minimum distance between any two occurrences of the minimum integer in the array.
 // Input: Array A[0..n -1] of numbers.
 // Output: Minimum distance between two of its elements.
 // Reference(s): https://www.geeksforgeeks.org/distance-two-closest-minimum/
@@ -11,9 +10,8 @@ int main()
 {
     int arrayOfNumbers[] = { 5, 1, 2, 3, 4, 1, 2, 1 };
 
-    // Get the size of the array.
-    // sizeof() operator returns the size of a type in bytes.
-    // To find out how many elements an array has, divide the size of the array by the size of the data type it.
+    // Get the size of the array. The sizeof() operator returns the size of a type in bytes. To find out how many
+    // elements an array has, divide the size of the array by the size of the data type it.
     int totalNumberOfElements = sizeof(arrayOfNumbers) / sizeof(int);
     int minimumDistance = INT_MAX;
 
